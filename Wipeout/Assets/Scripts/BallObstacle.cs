2e8e06ee4196f9d46f3d5c81d20dc82cc1d91e7e
HyeongJae <32874108+HyeongJae@users.noreply.github.com>
2020-12-13 04:38:33 +0900

    12-13
    obstacle1

---
+++ Wipeout/Assets/Scripts/BallObstacle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallObstacle : MonoBehaviour
+{
+    
+    public Vector3 origin;
+
+    public Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody.velocity = new Vector3(0f, 0f, 0f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.z <= 250.0)
+        {
+            rigidbody.velocity = new Vector3(0f, 0f, 0f);
+            transform.position = origin;
+
+            
+
+
+        }
+    }
+}
