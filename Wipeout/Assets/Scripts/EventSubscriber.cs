b8446dd9235ef56346b84e0bd43edbdafdf8eb0c
Park Du Hwan <pdh930318@gmail.com>
2020-12-13 05:19:10 +0900

    시작화면제작
    합병및 시작화면제작

---
+++ Wipeout/Assets/Scripts/EventSubscriber.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class EventSubscriber : MonoBehaviour
+{
+    public Button startButton;
+    public Button endButton;
+    private float startTime = 0f;
+    private bool isPressedStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //startButton = GetComponent<Button>();
+        //endButton = GetComponent<Button>();
+        //// 스타트 버튼이 클릭되었을 때 유니티 메서드 호출
+        startButton.onClick.AddListener(OnClickedStart);
+        endButton.onClick.AddListener(OnClickedExit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressedStart)
+        {
+            startTime += Time.deltaTime * 3f;
+            if (startTime >= 3.5f)
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+
+    //
+    private void OnClickedStart()
+    {
+        // loadScene();
+        isPressedStart = true;
+    }
+    private void OnClickedExit()
+    {
+        Application.Quit();
+    }
+
+}
