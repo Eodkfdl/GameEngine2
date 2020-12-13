d45a772733f0aecd3b169c02ea1c7e8f280e9d3a
Park Du Hwan <pdh930318@gmail.com>
2020-12-13 18:30:28 +0900

    싹밀고 다시올림
    새로클론해서 써주길바라

---
+++ Wipeout/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    public int sign;
+
+    public float speed;
+
+    public float minX, maxX;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(speed * Time.deltaTime * sign,0,0);
+        if(transform.position.x <= minX || transform.position.x >= maxX)
+        {
+            sign *= -1;
+        }
+    }
+
+}
